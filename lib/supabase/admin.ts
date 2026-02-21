eeb4fc3d7da61439a16a5af1b6aa86003a1c8d19
Andre Lee <realandre1234@gmail.com>
2026-02-21 03:34:20 -0800

    Fix Typo

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 //Admin operations that bypass RLS
 export const adminClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.UPABASE_PUBLISHABLE_KEY!
+  process.env.SUPABASE_PUBLISHABLE_KEY!
 )
