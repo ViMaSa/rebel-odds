8ffd77c551f25229c5a4f333d759c71fea9a170d
Andre Lee <realandre1234@gmail.com>
2026-02-21 03:29:14 -0800

    Fix depedencies and keys

--- lib/supabase/admin.ts
+++ lib/supabase/admin.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 //Admin operations that bypass RLS
 export const adminClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.UPABASE_PUBLISHABLE_KEY!
 )
